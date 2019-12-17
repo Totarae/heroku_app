fdc7c93131d4f340a7eef085f5a2d5b16869de73
Unknown <gregoryivanoff6@gmail.com>
2019-12-17 22:02:34 +0300

    Init

---
+++ src/main/java/App.java
@@ -0,0 +1,25 @@
+import freemarker.template.Configuration;
+
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+    public static void main(String[] args) {
+        final Configuration cfg = new Configuration();
+        cfg.setClassForTemplateLoading(App.class, "/");
+        cfg.setDefaultEncoding("UTF-8");
+
+        port(getHerokuAssignedPort());
+
+        get("/", (req, res) -> "Hello World");
+    }
+}
